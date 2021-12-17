e8b0736a27981455a207c1b979e2b053627daaca
Miguel <zadel88@gmail.com>
2021-12-17 09:46:15 -0500

    Lists

---
+++ pyInsurance.py
@@ -0,0 +1,21 @@
+import csv
+#empty lists to organize data
+age =[]
+sex =[]
+bmi =[]
+children =[]
+smoker =[]
+region =[]
+charges =[]
+#load data
+insurance = open('insurance.csv','r')
+file = csv.DictReader(insurance)
+#appending to lists
+for col in file:
+    age.append(col['age'])
+    sex.append(col['sex'])
+    bmi.append(col['bmi'])
+    children.append(col['children'])
+    smoker.append(col['smoker'])
+    region.append(col['region'])
+    charges.append(col['charges'])                               
